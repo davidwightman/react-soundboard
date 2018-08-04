4b2f1ae58aabfc82c6edbce40d648a56402973a6
David Wightman <david@davidwightman.org>
2018-08-04 18:41:46 -0400

    added nowPlay method

--- src/Button.js
+++ src/Button.js
@@ -1,13 +1,22 @@
 import React, { Component } from 'react';
 
 class Button extends Component {
+    nowPlay = e => {
+        console.log('now play function in button ' + e.key)
+        this.props.onKeyDown(e.target.value);
+    }
 
 	render(){
-        console.log(this.props)
+        //console.log(this.props)
         
         return (
             <div>
-                    <div data-key={this.props.dataKey} className="key" onClick={this.props.onClick}>
+                    <div 
+                        data-key={this.props.dataKey}
+                        className="key" 
+                        onKeyPress={(e) => this.nowPlay(e)}
+                        tabIndex="1"
+                    >
                         <kbd>{this.props.letter}</kbd>
                     <span className="sound">{this.props.sound}</span>
                 </div>
